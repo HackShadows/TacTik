ccecd11e227a3938478b0cd5993c93c3b2cf515d
Marius Ciserane p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-03-20 14:58:09 +0100

    joueur.cpp

--- src/Joueur.cpp
+++ src/Joueur.cpp
@@ -9,12 +9,18 @@
 using namespace std;
 
 
-Joueur::Joueur(): pseudo("jean"), couleur(0){
+Joueur::Joueur(): pseudo("Jean"), couleur(0){
     for (int i = 0; i<4; i++){
         main[i] = nullptr;
         maison[i] = false;
     }
+}
 
+Joueur::Joueur(int id_couleur): pseudo("Jean"), couleur(id_couleur){
+    for (int i = 0; i<4; i++){
+        main[i] = nullptr;
+        maison[i] = false;
+    }
 }
 
 Joueur::~Joueur() {
@@ -23,10 +29,13 @@ Joueur::~Joueur() {
     }
 }
 
-void Joueur::setJoueur(string nom, int id_couleur){
-    assert(1 <= id_couleur && id_couleur <=6);
+void Joueur::setPseudo(string nom){
     pseudo = nom;
-    couleur = id_couleur;
+}
+
+void Joueur::setMaison(int indice, bool etat) {
+    assert(0 <= indice && indice < 4);
+    maison[indice] = etat;
 }
 
 void Joueur::piocherCarte(int indice, Carte * carte){
@@ -35,6 +44,7 @@ void Joueur::piocherCarte(int indice, Carte * carte){
 }
 
 Carte* Joueur::jouerCarte(int valeur) {
+    assert(-1 <= valeur && valeur <= 13);
     for (int i = 0; i<4; i++){
         Carte* carte = main[i];
         if (carte != nullptr && valeur == carte->getValeur()) {
@@ -62,9 +72,9 @@ void Joueur::testRegression(){
     }
     cout << "Constructeur valide !" << endl;
 
-    joueur.setJoueur("Toto", 3);
-    assert(joueur.pseudo == "Toto" && joueur.couleur == 3);
-    cout << "setJoueur valide !" << endl;
+    joueur.setPseudo("Toto");
+    assert(joueur.pseudo == "Toto");
+    cout << "setPseudo valide !" << endl;
 
     Carte carte_tmp;
     carte_tmp.setCarte(12);
@@ -73,6 +83,10 @@ void Joueur::testRegression(){
     cout << "piocherCarte valide !" << endl;
 
     Carte * carte = joueur.jouerCarte(12);
+    assert(carte->getValeur() == 12 && joueur.main[3] == nullptr);
+    cout << "jouerCarte valide !" << endl;
+
+    bool remplie = joueur.maisonRemplie();
     assert(carte->getValeur() == 12 && joueur.main[3] == nullptr);
     cout << "jouerCarte valide !" << endl;
 }
