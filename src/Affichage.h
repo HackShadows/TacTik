70b4b4474e6c53c903dd1fb6da100a5f8946a227
Valentin <valentin.laporte42@gmail.com>
2025-03-23 19:48:55 +0100

    docu affichage

--- src/Affichage.h
+++ src/Affichage.h
@@ -2,6 +2,140 @@
 #include "Jeu.h"
 
 /**
+* @brief Affiche un nombre prenant 2 espaces dans le terminal.
+*
+* @param id Le nombre que l'on veut afficher
+*/
+void affichageId(int id);
+
+/**
+* @brief Affiche la maison d'une couleur dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+* @param couleur la couleur dont on veut afficher la maison
+*/
+void affichageMaison(const Jeu & jeu, int couleur);
+
+/**
+* @brief Affiche la premiere ligne d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void ligneHaut4p(const Jeu & jeu);
+
+/**
+* @brief Affiche la deuxieme ligne d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void maisonHaut4p(const Jeu & jeu);
+
+/**
+* @brief Affiche la troisieme ligne d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void reserveHaut4p(const Jeu & jeu);
+
+/**
+* @brief Affiche les lignes du milieu d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void milieu4p(const Jeu & jeu);
+
+/**
+* @brief Affiche les reserves sur la 3eme ligne du bas d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void reserveBas4p(const Jeu & jeu);
+
+/**
+* @brief Affiche les maisons sur la deuxieme ligne du bas d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void maisonBas4p(const Jeu & jeu);
+
+/**
+* @brief Affiche la dernière ligne d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void ligneBas4p(const Jeu & jeu);
+
+/**
+* @brief Affiche la premiere ligne d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void ligneHaut6p(const Jeu & jeu);
+
+/**
+* @brief Affiche la deuxieme ligne d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void maisonHaut6p(const Jeu & jeu);
+
+/**
+* @brief Affiche la troisieme ligne d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void reserveHaut6p(const Jeu & jeu);
+
+/**
+* @brief Affiche les lignes du milieu d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void milieu6p(const Jeu & jeu);
+
+/**
+* @brief Affiche les reserves sur la 3eme ligne du bas d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void reserveBas6p(const Jeu & jeu);
+
+/**
+* @brief Affiche les maisons sur la deuxieme ligne du bas d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void maisonBas6p(const Jeu & jeu);
+
+/**
+* @brief Affiche la dernière ligne d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void ligneBas6p(const Jeu & jeu);
+
+/**
+* @brief Affiche la grille entière d'une partie 4 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void grille4p(const Jeu & jeu);
+
+/**
+* @brief Affiche la grille entière d'une partie 6 joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void grille6p(const Jeu & jeu);
+
+/**
+* @brief Affiche les cartes des joueurs dans le terminal.
+*
+* @param jeu Le jeu que l'on veut afficher
+*/
+void affichageCarte(const Jeu & jeu);
+
+/**
 * @brief Affiche l'état du jeu sous format texte dans le terminal.
 *
 * @param jeu Le jeu à afficher.
