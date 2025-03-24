d6054d48f3fb1e05ac58cdd8efe3914a46d72501
Valentin <valentin.laporte42@gmail.com>
2025-03-24 18:24:17 +0100

    affichage couleur * joueurActif

--- src/txt/Affichage.h
+++ src/txt/Affichage.h
@@ -19,78 +19,98 @@ string intToStr(int entier);
 * @brief Affiche la maison d'une couleur dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
-* @param couleur la couleur dont on veut afficher la maison
+* @param nbCase Le nombre de case du plateau
+* @param couleur La couleur dont on veut afficher la maison
 */
-void affichageMaison(const Jeu & jeu, int couleur);
+void affichageMaison(const Jeu & jeu, int nbCase, int couleur);
 
 /**
 * @brief Affiche la premiere ligne d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
+* @param joueurActif Le joueur qui joue
 */
-void ligneHaut(const Jeu & jeu);
+void ligneHaut(const Jeu & jeu, int nbCase, int joueurActif);
 
 /**
 * @brief Affiche la deuxieme ligne d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
+* @param joueurActif Le joueur qui joue
+
 */
-void maisonHaut(const Jeu & jeu);
+void maisonHaut(const Jeu & jeu, int nbCase, int joueurActif);
 
 /**
 * @brief Affiche la troisieme ligne d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
+* @param joueurActif Le joueur qui joue
 */
-void reserveHaut(const Jeu & jeu);
+void reserveHaut(const Jeu & jeu, int nbCase, int joueurActif);
 
 /**
 * @brief Affiche les lignes du milieu d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
 */
-void milieu(const Jeu & jeu);
+void milieu(const Jeu & jeu, int nbCase);
 
 /**
 * @brief Affiche les reserves sur la 3eme ligne du bas d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
+* @param joueurActif Le joueur qui joue
 */
-void reserveBas(const Jeu & jeu);
+void reserveBas(const Jeu & jeu, int nbCase, int joueurActif);
 
 /**
 * @brief Affiche les maisons sur la deuxieme ligne du bas d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
+* @param joueurActif Le joueur qui joue
 */
-void maisonBas(const Jeu & jeu);
+void maisonBas(const Jeu & jeu, int nbCase, int joueurActif);
 
 /**
 * @brief Affiche la dernière ligne d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
+* @param joueurActif Le joueur qui joue
 */
-void ligneBas(const Jeu & jeu);
+void ligneBas(const Jeu & jeu, int nbCase, int joueurActif);
 
 
 /**
 * @brief Affiche la grille entière d'une partie dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param nbCase Le nombre de case du plateau
+* @param joueurActif Le joueur qui joue
 */
-void grille(const Jeu & jeu);
+void grille(const Jeu & jeu, int nbCase, int joueurActif);
 
 
 /**
 * @brief Affiche les cartes des joueurs dans le terminal.
 *
 * @param jeu Le jeu que l'on veut afficher
+* @param indiceJoueur L'indice du joueur dont on veut afficher les cartes.
+* @param joueurActif Le joueur qui joue
 */
-void affichageCarte(const Jeu & jeu);
+void affichageCarte(const Jeu & jeu, int indiceJoueur, int joueurActif);
 
 /**
 * @brief Affiche l'état du jeu sous format texte dans le terminal.
 *
 * @param jeu Le jeu à afficher.
+* @param joueurActif Le joueur qui joue
 */
-void affichageTexte(const Jeu & jeu);
+void affichageTexte(const Jeu & jeu, int joueurActif);
