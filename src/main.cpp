f1bb88eca0142d3169ec6d2907b1f8a8a0e2712f
p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-03-24 18:17:58 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -18,45 +18,15 @@ int main(){
 	Jeu jeu(nbJoueurs);
     jeu.distribuer();
     affichageTexte(jeu);
-	cout << "'Entrée' pour passer à l'étape suivante" << endl;
-	cin.get();
-	cin.get();
 	int ordre[6] = {1, 2, 5, 3, 4, 6};
-	/*
-	if (nbJoueurs == 4) {
-		for (int i = 0 ; i < nbJoueurs ; i++) {
-			jeu.demarrer(i+1);
-			affichageTexte(jeu);
-			cout << "'Entrée' pour passer à l'étape suivante" << endl;
-			cin.get();
-		}
-	} else {
-		for (int i : ordre) {
-			jeu.demarrer(i);
-			affichageTexte(jeu);
-			cout << "'Entrée' pour passer à l'étape suivante" << endl;
-			cin.get();
-		}
-	}
-	
-	jeu.avancerPion(3, 1);
-	affichageTexte(jeu);
-	cout << "'Entrée' pour passer à l'étape suivante" << endl;
-	cin.get();
-	jeu.avancerPion(13, 13);
-	affichageTexte(jeu);
-	cout << "'Entrée' pour passer à l'étape suivante" << endl;
-	cin.get();
-	jeu.avancerPion(7, 13);
-	affichageTexte(jeu);*/
 	int it = 0, val_carte, couleur;
 	while (it < 10) {
-		couleur = ordre[it%nbJoueurs];
+		couleur = (nbJoueurs == 6) ? ordre[it%nbJoueurs]:it%nbJoueurs+1;
 		cout << "\nTour de " << intToStr(couleur-1) << " :";
 		cout << "\nCarte à jouer : ";
 		cin >> val_carte;
 		if(!jeu.jouerCarte(val_carte, couleur)) jeu.defausserCarte(val_carte, couleur);
-		affichageTexte(jeu);
+		affichageTexte(jeu, couleur);
 		it++;
 	}
     return 0;
