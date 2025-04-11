b60bdfa8b5fa887073eb00caea229e56bd57907c
p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-04-11 20:12:45 +0200

    vidage du mainDev

--- src/mainDEV.cpp
+++ src/mainDEV.cpp
@@ -13,25 +13,10 @@ int main() {
     #endif
 
 	srand(time(NULL));
-	//int vainqueurs = jouer(true);
 	
 	/* Début de l'espace modifiable */	
 	
-	Jeu jeu(4);
-	jeu.distribuer();
-	affichageTexte(jeu, 7);
-	IA ia;
-	for (int j = 0 ; j < 4 ; j++) {
-		for (int i = 0 ; i < 4 ; i++) {
-			cout << intToStr(i) << " : ";
-			vector<pair<int, int>> vect = ia.genererCoups(jeu, i+1);
-			for (auto val : vect) cout << '(' << val.first << " ; " << val.second << ')';
-			cout << endl;
-		}
-		
-		cin.get();
-		affichageTexte(jeu, 7);
-	}
+	
 
 	/* Fin de l'espace modifiable */
 		
