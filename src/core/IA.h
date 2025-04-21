c8302da443aac26f56fd678da6b4f50a0fc8945f
LAPORTE VALENTIN p2302750 <valentin.laporte@etu.univ-lyon1.fr>
2025-04-21 15:49:59 +0000

    Edit IA.h

--- src/core/IA.h
+++ src/core/IA.h
@@ -20,37 +20,42 @@ class Jeu;
 * @brief Représente une ia.
 */
 class IA {
-		
-	public:
-		/**
-		* @brief Constructeur de l'ia.
-		*/
-		IA();
 
-		/**
-		* @brief Destructeur de l'ia.
-		*/
-		~IA();
+public:
+	/**
+	* @brief Constructeur de l'ia.
+	*/
+	IA();
 
-		/**
-		* @brief Permet à l'ia d'effectuer un coups parmis ses différents coups possibles.
-		*/
-		void jouerCoup(Jeu &jeu, int couleur) const;
+	/**
+	* @brief Destructeur de l'ia.
+	*/
+	~IA();
 
-		/**
-		* @brief Génère la liste des coups possibles.
-		*
-		* @return Liste de paires de coups possible contenant l'id du pion et sa nouvelle position.
-		*/
-		vector<pair<int, int>> genererCoups(Jeu &jeu, int couleur) const;
+	/**
+	* @brief Permet à l'ia d'effectuer un coups parmis ses différents coups possibles.
+	*/
+	void jouerCoup(Jeu &jeu, vector<pair<int, int>> vect, int couleur) const;
 
-		/**
-		* @brief Teste les fonctionnalités de la classe.
-		*
-		* Effectue une série de tests pour vérifier que toutes les fonctions produisent uniquement le résultat attendu,
-		* et que les données membres de l'objet sont conformes en tout temps.
-		*/
-		static void testRegression();
+	/**
+	* @brief Permet à l'ia d'effectuer un coups parmis ses différents coups possibles.
+	*/
+	int jouerCarte(Jeu &jeu, int indice) const;
+
+	/**
+	* @brief Génère la liste des coups possibles.
+	*
+	* @return Liste de paires de coups possible contenant l'id du pion et sa nouvelle position.
+	*/
+	vector<pair<int, int>> genererCoups(Jeu &jeu, int couleur) const;
+
+	/**
+	* @brief Teste les fonctionnalités de la classe.
+	*
+	* Effectue une série de tests pour vérifier que toutes les fonctions produisent uniquement le résultat attendu,
+	* et que les données membres de l'objet sont conformes en tout temps.
+	*/
+	static void testRegression();
 };
 
 #endif
